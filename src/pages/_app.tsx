19a8cd7d7419d6dc6828460feba0235769cfa9bd
DevMiguell <buguel2000@gmail.com>
2023-03-29 08:13:04 -0300

    feat: init project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Native WebRTC API with NextJS</title>
+        <meta name="description" content="Use Native WebRTC API for video conferencing" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  )
 }
